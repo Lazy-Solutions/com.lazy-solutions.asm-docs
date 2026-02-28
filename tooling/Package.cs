6b23ac23a6351a82b7fc4529b483092a89416276
Andreas Ingeholm <andreas.ingeholm@gmail.com>
2026-02-28 18:25:49 +0100

    Update Package.cs

--- tooling/Package.cs
+++ tooling/Package.cs
@@ -12,7 +12,8 @@ namespace AdvancedSceneManager.Documentation
         public const string repoUrl = "https://github.com/Lazy-Solutions/com.lazy-solutions.asm-docs";
         public const string packageName = "com.lazy-solutions.asm-docs";
         public static string folder { get; } = $"Packages/{packageName}";
-        public static string apiFolder { get; } = $"Packages/{packageName}/API";
+        public static string apiFolder { get; } = $"Packages/{packageName}/api";
+        public static string guidesFolder { get; } = $"Packages/{packageName}/guides";
         public static string readMePath { get; } = $"Packages/{packageName}/readme.asset";
     }
 
