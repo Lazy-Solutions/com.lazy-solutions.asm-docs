91b38efbe7624efa93318913d02a6773a0cfebba
Andreas Ingeholm <andreas.ingeholm@gmail.com>
2026-05-23 13:17:10 +0200

    Update Package.cs

--- tooling/Package.cs
+++ tooling/Package.cs
@@ -1,16 +1,10 @@
-﻿#if CSPROJ_ATTRIBUTES
-using AdvancedSceneManager.CsProjAttributes;
-using AdvancedSceneManager.Documentation;
-[assembly: Include("Packages/" + Package.packageName + "/**/*.md")]
-#endif
-
-namespace AdvancedSceneManager.Documentation
+﻿namespace AdvancedSceneManager.Documentation
 {
 
     public static class Package
     {
-        public const string repoUrl = "https://github.com/Lazy-Solutions/com.lazy-solutions.asm-docs";
         public const string packageName = "com.lazy-solutions.asm-docs";
+        public static string repoUrl { get; } = $"https://github.com/Lazy-Solutions/{packageName}";
         public static string folder { get; } = $"Packages/{packageName}";
         public static string apiFolder { get; } = $"Packages/{packageName}/api";
         public static string guidesFolder { get; } = $"Packages/{packageName}/guides";
