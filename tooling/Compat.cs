c7bb8f517d30d934e865fe1bdac3f745f7c58ee0
Andreas Ingeholm <andreas.ingeholm@gmail.com>
2026-07-28 09:39:20 +0200

    Added shims for [NoAutoStaticsCleanup] and [AutoStaticsClean]

---
+++ tooling/Compat.cs
@@ -0,0 +1,21 @@
+﻿#region AutoStaticsCleanupAttribute
+
+//These two attributes were introduced in 6.5, and are required (via import warnings).
+//Our min version is 6.0, so we need to define dummy ones in earlier versions, unless we want hundreds of version defines everywhere.
+
+#if !UNITY_6000_5_OR_NEWER
+namespace Unity.Scripting.LifecycleManagement
+{
+
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Event | AttributeTargets.Field | AttributeTargets.Property | AttributeTargets.Struct, AllowMultiple = false)]
+    internal sealed class NoAutoStaticsCleanupAttribute : Attribute
+    { }
+
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Event | AttributeTargets.Field | AttributeTargets.Property | AttributeTargets.Struct, AllowMultiple = true)]
+    internal sealed class AutoStaticsCleanupAttribute : Attribute
+    { }
+
+}
+#endif
+
+#endregion
